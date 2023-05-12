3efc41a20f00ae6719260670d0aed37646db3d7e
AlexSobral01 <alex.dacostaSobral@gmail.com>
2023-05-12 12:06:05 -0300

    start project pokedex

---
+++ src/pages/about.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div style={{ color: "white", padding: "15px", maxWidth: "80vw", margin: "auto" }}>
+      <h1 style={{margin: "15px 0px"}}>About</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam officiis aut cum, numquam, vel eos debitis minima nostrum nihil dolorem corrupti rerum dignissimos obcaecati reprehenderit perspiciatis mollitia eius error consequatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum temporibus adipisci quod officia minus culpa corporis tenetur doloribus! Numquam commodi sit officiis autem rem placeat iure dolorem explicabo mollitia. Explicabo?</p>
+    </div>
+  )
+}
---
+++ src/pages/contact.jsx
@@ -0,0 +1,9 @@
+export default function contact() {
+  return (
+    <div style={{ color: "white", padding: "15px", maxWidth: "80vw", margin: "auto" }}>
+      <h1 style={{margin: "15px 0px"}}>Contact</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, totam</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quod labore, hic dolores quis, pariatur ratione facere error corporis nobis sint quisquam reiciendis itaque libero nisi omnis voluptas ipsam repudiandae.</p>
+    </div>
+  )
+}
